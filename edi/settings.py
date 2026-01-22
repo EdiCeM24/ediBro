b61e2deebeb0c3b83130221b0dc3e7a285ab7bfb
Marcus Edidiong Cletus <edi_mars@outlook.com>
2026-01-22 22:33:02 +0100

    Add gunicorn to repo.

--- edi/settings.py
+++ edi/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', '.now_sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.ediBro.render.com', '127.0.0.1', 'localhost']
 
 
 
