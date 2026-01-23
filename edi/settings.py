8f07cca54aa2b50296bb1a131f54c4856ef2c5ce
Marcus Edidiong Cletus <edi_mars@outlook.com>
2026-01-23 08:32:25 +0100

    Add changes

--- edi/settings.py
+++ edi/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.ediBro.render.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ediBro.render.com', '127.0.0.1', 'localhost']
 
 
 
